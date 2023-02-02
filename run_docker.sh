3355451594dd24ebe087924375be1f80491301a0
Sergi <sergi.saya96@gmail.com>
2023-02-02 14:49:01 +0000

    update run_docker network capabilities

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,9 @@
-docker run --net=host -it \
+docker run -it \
+    --net=host \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --device=/dev/dri:/dev/dri \
+    --privileged \
     --name="b1_dev"\
     b1_dev
